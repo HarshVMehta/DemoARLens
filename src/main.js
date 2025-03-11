ea70f915edeae935f8ac50e682fdc0d2ee6e431a
Harsh Mehta <142716187+HarshVMehta@users.noreply.github.com>
2025-03-11 21:27:26 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import {
 (async function () {
     try {
         var cameraKit = await bootstrapCameraKit({
-            apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM5MjY0NzIyLCJzdWIiOiJlYzNkMmQ0NS02YTA0LTQ3YzQtOGQ2Ni0wZWQ5Y2I1YmQxNDZ-U1RBR0lOR343MGM2YjhkNy1hNDdhLTQ1MjYtOWIyNy02ZDQ0MTAzY2RlOWQifQ.8khOqHCknMG9HQS8QIBfYOQvxFe5ahbmqnD3QGlKj_M'
+            apiToken: api
         });
 
         const session = await cameraKit.createSession();
